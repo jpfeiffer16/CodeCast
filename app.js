68d4fc9bd0be19c9b802eb5a8ae8c1b4e9357b31
jpfeiffer16 <jpfeiffer@bluemodus.com>
2015-12-05 23:14:50 -0700

    Adding websockets and default page

--- app.js
+++ app.js
@@ -1,4 +1,31 @@
+var port = 3000;
+
 var fileWatcher = require('./app/fileWatcher');
+var http = require('http');
+var fs = require('fs');
 
 
 fileWatcher.watchFile('./test.js');
+
+
+var app = http.createServer(function(req, res) {
+  // res.end('Hi');
+  fs.readFile('./page.html', 'utf8', function(err, data) {
+    if (!err) {
+      res.end(data);
+    } else {
+      res.end('Error: ' + err.message);
+    }
+  });
+}).listen(port, '127.0.0.1', function() {
+  console.log('Server listening on port: ' + port);
+});
+
+var io = require('socket.io')(app);
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
--- app/fileWatcher.js
+++ app/fileWatcher.js
@@ -6,15 +6,16 @@ var fileState = [];
 
 process.on('SIGINT', function() {
   console.log('Bye bye');
-  console.dir(fileState);
-  process.exit();
+  // console.dir(fileState);
+  fs.writeFile('./output.ccst', JSON.stringify(fileState), 'utf8', function(err) {
+    if (!err) {
+      console.log('File written successfully');
+    } else {
+      console.log('Error writing file: ' + err.message);
+    }
+    process.exit();
+  });
 });
-
-
-
-
-
-
 
 module.exports = (function() {
   function watchFile(filename) {
